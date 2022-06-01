c1d625091763ecd2d7f457a740772231d24cab81
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-06-02 00:05:43 +0900

    feat: zustand state setting

---
+++ src/features/store.ts
@@ -0,0 +1,43 @@
+import produce from 'immer'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+import { devtools } from 'zustand/middleware'
+
+interface IStore {
+  // auth
+  token: string | null
+  setToken: (token: string) => void
+  removeToken: () => void
+}
+
+const useStore = create<IStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        token: null,
+        setToken: (token: string) =>
+          set(
+            produce<IStore>(store => {
+              store.token = token
+            }),
+            false,
+            'setToken',
+          ),
+        removeToken: () =>
+          set(
+            produce<IStore>(store => {
+              store.token = null
+            }),
+            false,
+            'removeToken',
+          ),
+      }),
+      {
+        name: 'staticks-store',
+        getStorage: () => localStorage,
+      },
+    ),
+  ),
+)
+
+export default useStore
