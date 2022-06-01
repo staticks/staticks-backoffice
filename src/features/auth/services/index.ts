aa9edcf429bc02aef02160b734d1e04013674e01
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-06-02 00:07:29 +0900

    feat: set login service with react-query

--- src/features/auth/services/index.ts
+++ src/features/auth/services/index.ts
@@ -1,4 +1,51 @@
-// import axios from 'axios'
-// import { useQuery } from 'react-query'
+import { AxiosResponse } from 'axios'
+import { useQuery } from 'react-query'
+import Axios from '../../../utils/axiosUtil'
 
-export const authService = {}
+interface LoginPayload {
+  accountId: string
+  password: string
+  authentication: string
+}
+
+interface LoginResponse {
+  type: string
+  token: string
+}
+
+export const authService = {
+  async login(payload: LoginPayload) {
+    const res: AxiosResponse<LoginResponse> = await Axios.post('/auth/login', {
+      accountId: payload.accountId,
+      password: payload.password,
+      authentication: 'STATICKS',
+    })
+
+    return res.data
+  },
+}
+
+function useAuthService(payload: LoginPayload | any) {
+  const { data, error, isLoading, refetch } = useQuery(
+    [
+      'auth',
+      'login',
+      `accountId=${payload.accountId}`,
+      `password=${payload.password}`,
+    ],
+    () => authService.login(payload),
+    {
+      enabled: !!payload.accountId && !!payload.password,
+      staleTime: Infinity,
+      retry: false,
+    },
+  )
+  return {
+    data,
+    error,
+    isLoading,
+    refetch,
+  }
+}
+
+export default useAuthService
