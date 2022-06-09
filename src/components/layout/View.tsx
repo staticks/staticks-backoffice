a6d0e3151fdd646ad92acc6770f8c9ae5ee50192
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-06-09 20:17:33 +0900

    feat: ViewPage Component 분리 및 View 하위 컴포넌트 ui 수정

--- src/components/layout/View.tsx
+++ src/components/layout/View.tsx
@@ -1,14 +1,16 @@
+import { css } from '@emotion/react'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import tw from 'twin.macro'
 
 export interface ViewProps extends React.HTMLAttributes<HTMLDivElement> {
   hasOutlet?: boolean
+  page?: boolean
 }
 
 export const View: React.FC<ViewProps> = ({ hasOutlet, children }) => {
   return (
-    <div tw="bg-gray-50 flex justify-center items-center h-[100vh] overflow-y-auto">
+    <div tw="bg-gray-50 flex justify-center h-[100vh] overflow-y-auto">
       {hasOutlet ? <Outlet /> : children}
     </div>
   )
@@ -16,7 +18,28 @@ export const View: React.FC<ViewProps> = ({ hasOutlet, children }) => {
 
 export const ViewBox: React.FC<ViewProps> = ({ children, ...rest }) => {
   return (
-    <div css={[tw`border border-gray-200 bg-white rounded-lg p-8`]} {...rest}>
+    <div
+      css={[
+        tw`border border-gray-200 bg-white rounded-lg p-8 my-auto`,
+        css`
+          height: max-content;
+        `,
+      ]}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
+
+export const ViewPage: React.FC<ViewProps> = ({ children, ...rest }) => {
+  return (
+    <div
+      css={[
+        tw`border border-gray-200 bg-white rounded-lg p-8`,
+        tw`w-full max-w-[800px] mx-5 my-10 overflow-auto`,
+      ]}
+    >
       {children}
     </div>
   )
--- src/features/project/views/Projects.tsx
+++ src/features/project/views/Projects.tsx
@@ -1,14 +1,11 @@
-import { View, ViewBox } from '@/components/layout'
-import { Link } from 'react-router-dom'
-import { ExclamationCircleIcon } from '@heroicons/react/solid'
+import { View, ViewBox, ViewPage } from '@/components/layout'
+import tw from 'twin.macro'
+import { css } from '@emotion/react'
 
 const Projects = () => {
   return (
     <View>
-      <ViewBox>
-        <ExclamationCircleIcon tw="w-10 text-blue-600 mx-auto mb-4" />
-        <p tw="text-center">준비중입니다...</p>
-      </ViewBox>
+      <ViewPage></ViewPage>
     </View>
   )
 }
